2a557a982050778ff838e2031ab5eed58b9a2383
nGyu <vpdls1511@gmail.com>
2024-02-21 17:17:54 +0900

    fix : url 수정

--- src/utils/onFetchService.ts
+++ src/utils/onFetchService.ts
@@ -7,8 +7,8 @@ interface fetchParam {
 }
 
 const OnFetchService = <T>({url, method, data}: fetchParam): Promise<T> => {
-	const base_url = 'http://localhost:8001/api/'
-	// const base_url = 'http://202.31.253.176:8001/api/'
+	// const base_url = 'http://localhost:8001/api/'
+	const base_url = 'http://202.31.253.176:8001/api/'
 
 	/*return fetch(base_url + url, {
 		method: method,
